3f393dfa1ed44ebd17195b4d96776ab7e6aed7a0
Elijah <eramos@class-ic.com>
2026-09-23 14:29:34 -0700

    chore: adjusted imports to be uniform

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-from .database import Base
+from app.database import Base
 from datetime import datetime
 
 
